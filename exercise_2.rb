46f92721fd19958423856e7aff64ed6d2bb6bbe6
Abby Christens <achristens@gmail.com>
2017-09-25 14:52:52 -0400

    Adding initalize, reader, and writer methods

--- exercise_2.rb
+++ exercise_2.rb
@@ -1,5 +1,18 @@
 class BankAccount
-  # This class variable should be a float
+  # The interest_rate class variable should be a float
   @@interest_rate
   @@accounts = []
+
+  def initialize
+    @balance = 0
+  end
+
+  def balance
+    @balance
+  end
+
+  # The below may have to use a += or -= to update the balance
+  def change_balance(value)
+    @balance = value
+  end
 end
