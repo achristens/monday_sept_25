4e9d1f3c3437616a15d5acfb6ff07d26a1503326
Abby Christens <achristens@gmail.com>
2017-09-25 17:22:08 -0400

    Adding class methods

--- book_lending.rb
+++ book_lending.rb
@@ -1,6 +1,17 @@
 class Book
   @@on_shelf  = []
-  @@on_load   = []
+  @@on_loan   = []
+
+  def initialize(title, author, isbn)
+    @title  = title
+    @author = author
+    @isbn   = isbn
+  end
+
+  def self.create(title, author, isbn)
+    new_book = Book.new(title, author, isbn)
+    @@on_shelf << new_book
+  end
 
   def due_date
     @due_date
@@ -9,4 +20,16 @@ class Book
   def update_due_date(new_date)
     @due_date = new_date
   end
+
+  def self.available
+    return @@on_shelf
+  end
+
+  def self.borrowed
+    return @@on_loan
+  end
 end
+
+sister_outsider = Book.create("Sister Outsider", "Audre Lorde", "9781515905431")
+puts sister_outsider.inspect
+puts "Here is what is currently in stock: #{Book.available.inspect}"
