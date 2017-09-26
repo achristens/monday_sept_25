663ef1bdc3986a802fc96b0268d4b4ea06c39a5b
Abby Christens <achristens@gmail.com>
2017-09-25 20:47:11 -0400

    Setting up variables and methods

---
+++ zombies.rb
@@ -0,0 +1,49 @@
+class Zombie
+  # CLASS VARIABLES:
+  @@horde            = []
+  # This value will increase over time:
+  @@plague_level     = 10
+  # These values won't change:
+  @@max_speed        = 5
+  @@max_strength     = 8
+  @@default_speed    = 1
+  @@default_strength = 3
+
+  # INSTANCE METHODS
+  def @initialize
+
+  end
+
+  def encounter
+
+  end
+
+  def outrun_zombie?
+
+  end
+
+  def survive_attack?
+
+  end
+
+  # CLASS METHODS
+  def self.all
+
+  end
+
+  def self.new_day
+
+  end
+
+  def self.some_die_off
+
+  end
+
+  def self.spawn
+
+  end
+
+  def self.increase_plague_level
+
+  end
+end
